9848a7cb694015ca59a7fca29911d599ca5e9db9
mon_sat <tnk@daijiten.info>
2022-06-18 18:31:09 +0900

    refactor: import plugin file

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,31 +1,3 @@
-import type { PluginObject } from '@vuepress/core'
-import { path } from '@vuepress/utils'
+import { netabareSwitchPlugin } from './netabareSwitchPlugin'
 
-export interface NetaBarePluginOptions {
-  componentPrefix?: string
-  keyPrefix?: string
-}
-
-export const netabareSwitchPlugin = (options?: NetaBarePluginOptions): PluginObject => {
-  const name = 'vuepress-plugin-netabare-switch'
-  const clientConfigFile = path.resolve(
-    __dirname,
-    `../client/clientConfig.mjs`
-  )
-  const {
-    componentPrefix = 'NetaBare',
-    keyPrefix = 'netabare',
-  }: NetaBarePluginOptions = options || {}
-  const define = {
-    __NETABARE_COMPONENT_PREFIX__: componentPrefix,
-    __NETABARE_KEY_PREFIX__: keyPrefix,
-  }
-
-  return {
-    name,
-    clientConfigFile,
-    define,
-  }
-}
-
-export default netabareSwitchPlugin
+export * from './netabareSwitchPlugin'
---
+++ src/node/netabareSwitchPlugin.ts
@@ -0,0 +1,29 @@
+import type { PluginObject } from '@vuepress/core'
+import { path } from '@vuepress/utils'
+
+export interface NetaBarePluginOptions {
+  componentPrefix?: string
+  keyPrefix?: string
+}
+
+export const netabareSwitchPlugin = (options?: NetaBarePluginOptions): PluginObject => {
+  const name = 'vuepress-plugin-netabare-switch'
+  const clientConfigFile = path.resolve(
+    __dirname,
+    `../client/clientConfig.mjs`
+  )
+  const {
+    componentPrefix = 'NetaBare',
+    keyPrefix = 'netabare',
+  }: NetaBarePluginOptions = options || {}
+  const define = {
+    __NETABARE_COMPONENT_PREFIX__: componentPrefix,
+    __NETABARE_KEY_PREFIX__: keyPrefix,
+  }
+
+  return {
+    name,
+    clientConfigFile,
+    define,
+  }
+}
